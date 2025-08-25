1cc5e482278b85c35a66928407345faaea703898
Jessica <Jessica@LAPTOP-SDNI25TK>
2025-08-25 16:05:08 -0500

    hola yo

---
+++ src/practica/pkg1/jf/ejercicio3.java
@@ -0,0 +1,13 @@
+
+package practica.pkg1.jf;
+import java.util.Scanner;
+/**
+ *
+ * @author Juan Felipe Ramirez Marquez 20251232655
+ */
+public class ejercicio3 {
+    public static void main(String[] args){
+        
+    }
+    
+}
